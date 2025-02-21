36dd6431bea723344c6a84a0f2cefeeb29b7b64c
otesunki <otesunki@spaceshark.gay>
2025-02-21 20:13:07 +0000

    add more sections

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,6 +1,7 @@
-use crate::bufferedread::BufferedRead;
+use crate::read_tape::ReadTape;
 use std::error::Error;
 use std::io::{self, Read, Result};
+use std::marker::PhantomData;
 
 fn invalid_data(err: impl Into<Box<dyn Error + Send + Sync>>) -> io::Error {
     io::Error::new(io::ErrorKind::InvalidData, err)
@@ -14,51 +15,134 @@ macro_rules! expect {
         }
     }
 }
-trait Deserialize
+trait Decode
 where
     Self: Sized,
 {
-    fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self>;
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self>;
 }
 
-macro_rules! impl_deserialize_for_ints {
+#[derive(Debug)]
+struct TodoDecode<const N: usize>;
+impl<const N: usize> Decode for TodoDecode<N> {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        todo!("todo decode#{}", N)
+    }
+}
+
+impl Decode for bool {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        match u8::decode(file)? {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(invalid_data("boolean should only be 0 or 1")),
+        }
+    }
+}
+
+macro_rules! impl_decode_for_int {
     ($($ints:ident)*) => {
         $(
-            impl Deserialize for $ints {
-                fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-                    file.read_varint()
+            impl Decode for $ints {
+                fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+                    file.read_leb128()
                 }
             }
         )*
     }
 }
 
-impl_deserialize_for_ints!(u8 i8 u16 i16 u32 i32 u64 i64);
-
-macro_rules! deserializable_enum {
-    ($(enum $name:ident: $type: ty { $($variant:ident = $value:expr),* $(,)? })*) => {
+macro_rules! impl_decode_naive {
+    ($($types:ty)*) => {
         $(
-        #[repr($type)]
-        #[derive(Debug)]
-        enum $name {
-            $($variant = $value),*
+        impl Decode for $types {
+            fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+                file.read_chunk().map(<$types>::from_le_bytes)
+            }
         }
-        impl Deserialize for $name {
-            fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-                let variant = file.read_varint::<u8>()?;
+        )*
+    }
+}
+
+impl_decode_for_int!(u8 i8 u16 i16 u32 i32 u64 i64);
+impl_decode_naive!(f32 f64);
+
+impl<T: Decode> Decode for PhantomData<T> {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let _ = T::decode(file)?;
+        Ok(PhantomData)
+    }
+}
+
+impl Decode for usize {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        Ok(u32::decode(file)?.try_into().expect("usize too small"))
+    }
+}
+
+macro_rules! decodable {
+    {} => {};
+    {$(#[$($meta:tt)*])* enum $name:ident: $type: ty { $($variant:ident $(($($inner:ty),*))? = $value:expr),* $(,)? } $($next:tt)*} => {
+        #[repr($type)]
+        $(#[$($meta)*])*
+        enum $name {
+            $($variant $(($($inner,)*))?= $value),*
+        }
+        impl Decode for $name {
+            fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+                let variant = u8::decode(file)?;
                 match variant {
                     $(
-                    $value => Ok($name::$variant),
+                    $value => {
+                        Ok($name::$variant$((
+                            $(
+                            <$inner>::decode(file)?,
+                            )*
+                        ))?)
+                    },
                     )*
                     _ => Err(invalid_data(format!("invalid {}: {}", stringify!($name), variant))),
                 }
             }
         }
-        )*
+        decodable! { $($next)* }
+    };
+    {$(#[$($meta:tt)*])* struct $name:ident { $($field:ident : $type:ty),* $(,)? } $($next:tt)*} => {
+        $(#[$($meta)*])*
+        struct $name {
+            $(
+            $field: $type,
+            )*
+        }
+        impl Decode for $name {
+            fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+                $(
+                let $field = <$type>::decode(file)?;
+                )*
+                Ok(Self {
+                    $(
+                    $field,
+                    )*
+                })
+            }
+        }
+        decodable! { $($next)* }
+    };
+}
+
+#[derive(Debug)]
+struct CustomSection;
+
+impl Decode for CustomSection {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let length = usize::decode(file)?;
+        let _ = file.read(length)?;
+        Ok(CustomSection)
     }
 }
 
-deserializable_enum! {
+decodable! {
+    #[derive(Debug)]
     enum ValueType: u8 {
         I32 = 0x7F,
         I64 = 0x7E,
@@ -69,81 +153,221 @@ deserializable_enum! {
         ExternRef = 0x6F,
     }
 
-    enum SectionType: u8 {
-        Custom = 0,
-        Type = 1,
-        Import = 2,
-        Function = 3,
-        Table = 4,
-        Memory = 5,
-        Global = 6,
-        Export = 7,
-        Start = 8,
-        Element = 9,
-        Code = 10,
-        Data = 11,
-        DataCount = 12,
+    #[derive(Debug)]
+    enum Section: u8 {
+        Custom(CustomSection) = 0x00,
+        Type(Length, TypeSection) = 0x01,
+        Import(Length, ImportSection) = 0x02,
+        Function(Length, FunctionSection) = 0x03,
+        Table(Length, TableSection) = 0x04,
+        Memory(Length, MemorySection) = 0x05,
+        Global(Length, GlobalSection) = 0x06,
+        Export(Length, ExportSection) = 0x07,
+        Start(Length, TodoDecode<6>) = 0x08,
+        Element(Length, TodoDecode<7>) = 0x09,
+        Code(Length, TodoDecode<8>) = 0x0a,
+        Data(Length, TodoDecode<9>) = 0x0b,
+        DataCount(Length, TodoDecode<10>) = 0x0c,
     }
 }
 
-impl<T: Deserialize> Deserialize for Box<[T]> {
-    fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-        let len = u32::deserialize(file)?;
-        (0..len).map(|_| T::deserialize(file)).collect()
+type Length = PhantomData<usize>;
+
+impl<T: Decode> Decode for Box<[T]> {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let len = u32::decode(file)?;
+        (0..len).map(|_| T::decode(file)).collect()
     }
 }
 
 #[derive(Debug)]
-struct FuncType {
-    parameters: Box<[ValueType]>,
-    result: Box<[ValueType]>,
-}
-
-impl Deserialize for FuncType {
-    fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-        expect!(file.next_byte()? == 0x60, "invalid functype")?;
-        let parameters = Box::<[ValueType]>::deserialize(file)?;
-        let result = Box::<[ValueType]>::deserialize(file)?;
-        Ok(Self { parameters, result })
+struct TagByte<const TAG: u8>;
+impl<const TAG: u8> Decode for TagByte<TAG> {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        expect!(file.read_byte()? == TAG, "expected next byte to be {}", TAG)?;
+        Ok(Self)
     }
 }
 
-type TypeSection = Box<[FuncType]>;
+decodable! {
+    #[derive(Debug)]
+    struct FuncType {
+        _signature: TagByte<0x60>,
+        parameters: Box<[ValueType]>,
+        result: Box<[ValueType]>,
+    }
 
-impl Deserialize for &str {
-    fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-       let len = u32::deserialize(file);
+    #[derive(Debug)]
+    struct TypeSection {
+        functions: Box<[FuncType]>,
+    }
+}
+
+impl Decode for String {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let len = usize::decode(file)?;
+        Ok(dbg!(
+            String::from_utf8_lossy(&file.read(dbg!(len))?).into_owned()
+        ))
+    }
+}
+
+decodable! {
+    #[derive(Debug)]
+    struct Import {
+        module: String,
+        name: String,
+        description: InterfaceDescription,
+    }
+
+    #[derive(Debug)]
+    enum InterfaceDescription: u8 {
+        Func(u32) = 0x00,
+        Table(u32) = 0x01,
+        Memory(u32) = 0x02,
+        Global(u32) = 0x03,
+    }
+
+    #[derive(Debug)]
+    struct ImportSection {
+        imports: Box<[Import]>,
+    }
+
+    #[derive(Debug)]
+    struct Export {
+        name: String,
+        description: InterfaceDescription,
+    }
+
+    #[derive(Debug)]
+    struct ExportSection {
+        exports: Box<[Export]>,
+    }
+}
+
+type TypeIdx = u32;
+type MemoryType = Limit;
+type GlobalType = TodoDecode<103>;
+
+decodable! {
+    #[derive(Debug)]
+    struct FunctionSection {
+        signatures: Box<[TypeIdx]>,
+    }
+}
+
+decodable! {
+    #[derive(Debug)]
+    enum Limit: u8 {
+        HalfBounded(usize) = 0x00,
+        Bounded(usize, usize) = 0x01,
+    }
+
+    #[derive(Debug)]
+    struct TableType {
+        element_type: ValueType,
+        limits: Limit,
+    }
+
+    #[derive(Debug)]
+    struct TableSection {
+        tables: Box<[TableType]>,
+    }
+
+    #[derive(Debug)]
+    struct MemorySection {
+        memories: Box<[Limit]>,
+    }
+
+    #[derive(Debug)]
+    struct Global {
+        value_type: ValueType,
+        is_mutable: bool,
+        expression: Expression,
+    }
+
+    #[derive(Debug)]
+    struct GlobalSection {
+        globals: Box<[Global]>,
+    }
+
+    #[derive(Debug)]
+    enum Instruction: u8 {
+        Unreachable = 0x00,
+        Nop = 0x01,
+        Block(Expression) = 0x02,
+        Loop(Expression) = 0x03,
+        IfElse(IfElseBlock) = 0x04,
+        ConstI32(i32) = 0x41,
+        ConstI64(i64) = 0x42,
+        ConstF32(f32) = 0x43,
+        ConstF64(f64) = 0x44,
+    }
+}
+
+#[derive(Debug)]
+enum IfElseBlock {
+    If(Expression),
+    IfElse(Expression, Expression),
+}
+
+impl Decode for IfElseBlock {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let mut ifso = vec![];
+
+        while !matches!(file.peek_byte()?, INSTRUCTION_ELSE | INSTRUCTION_END) {
+            ifso.push(Instruction::decode(file)?);
+        }
+
+        let ifso = Expression {
+            instructions: ifso.into_boxed_slice(),
+        };
+        if file.read_byte()? == INSTRUCTION_ELSE {
+            let ifnot = Expression::decode(file)?;
+            Ok(IfElseBlock::IfElse(ifso, ifnot))
+        } else {
+            Ok(IfElseBlock::If(ifso))
+        }
+    }
+}
+
+const INSTRUCTION_ELSE: u8 = 0x05;
+const INSTRUCTION_END: u8 = 0x0B;
+
+#[derive(Debug)]
+struct Expression {
+    instructions: Box<[Instruction]>,
+}
+
+impl Decode for Expression {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        let mut instructions = vec![];
+
+        while file.peek_byte()? != INSTRUCTION_END {
+            instructions.push(Instruction::decode(file)?);
+        }
+        let _ = file.read_byte();
+
+        Ok(Expression {
+            instructions: instructions.into_boxed_slice(),
+        })
     }
 }
 
 pub struct Binary;
 
-impl Deserialize for Binary {
-    fn deserialize(file: &mut BufferedRead<impl Read>) -> Result<Self> {
-        expect!(file.next_chunk()? == *b"\0asm", "invalid data")?;
+impl Decode for Binary {
+    fn decode(file: &mut ReadTape<impl Read>) -> Result<Self> {
+        expect!(file.read_chunk()? == *b"\0asm", "invalid data")?;
         expect!(
-            u32::from_le_bytes(file.next_chunk()?) == 1,
+            u32::from_le_bytes(file.read_chunk()?) == 1,
             "unsupported WASM version"
         )?;
-        let type_section = None;
-        let import_section = None;
-        loop {
-            let section_type = SectionType::deserialize(file)?;
-            let length = u32::deserialize(file)?
-                .try_into()
-                .expect("u32 should be larger than a usize");
-            match section_type {
-                SectionType::Custom => {
-                    let _ = file.read(length);
-                }
-                SectionType::Type => {
-                    type_section = Some(TypeSection::deserialize(file)?);
-                }
-                SectionType::Import => {
-                    import_section = Some(ImportSection::deserialize(file)?);
-                }
-                _ => todo!("implement {:?} sections", section_type),
-            }
+        let mut sections = vec![];
+        while dbg!(file.has_data()?) {
+            let section = Section::decode(file)?;
+            println!("{:?}", section);
+            sections.push(section);
         }
 
         Ok(Binary)
@@ -151,6 +375,6 @@ impl Deserialize for Binary {
 }
 
 pub fn parse_file(file: impl Read) -> Result<Binary> {
-    let mut file = BufferedRead::new(file);
-    Binary::deserialize(&mut file)
+    let mut file = ReadTape::new(file);
+    Binary::decode(&mut file)
 }
